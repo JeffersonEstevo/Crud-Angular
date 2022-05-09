c27c2cc721abdfe6d43de21d10bd5c0c67203a95
JeffersonEstevo <jefferson.estevo@gmail.com>
2022-05-08 23:01:01 -0300

    segundo commit

--- src/app/tarefas/services/tarefas.service.ts
+++ src/app/tarefas/services/tarefas.service.ts
@@ -1,18 +1,23 @@
-import { Tarefa } from './../model/tarefa';
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Tarefa } from '../model/tarefa';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TarefasService {
 
+  private readonly API = '/assets/tarefas.json';
+
+
   constructor(private httpClient: HttpClient) { }
 
-  list(): Tarefa[] {
-    return [
-      {_id: 1, titulo : 'Limpar quarto', descricao: 'Dar a geral no quarto', responsavel: 'Jefferson',
-      prioridade: 'Alta', data: '08/05/2022' }
-    ];
+  list() { //: Tarefa[] {
+    return this.httpClient.get<Tarefa[]>(this.API);
+    /*return [
+      { "_id": 1, "titulo": "Limpar quarto", "descricao": "Dar a geral no quarto", "responsavel": "Jefferson",
+    "prioridade": "Alta", "data": "08/05/2022" }
+    ];*/
   }
 }
--- src/app/tarefas/tarefas/tarefas.component.ts
+++ src/app/tarefas/tarefas/tarefas.component.ts
@@ -1,6 +1,8 @@
-import { TarefasService } from './../services/tarefas.service';
-import { Tarefa } from './../model/tarefa';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Tarefa } from './../model/tarefa';
+import { TarefasService } from './../services/tarefas.service';
 
 @Component({
   selector: 'app-tarefas',
@@ -9,23 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TarefasComponent implements OnInit {
 
-  // podemos criar a tarefa assim, ou no construtor abaixo
-  tarefas: Tarefa[] = [
-    //{_id: 1, titulo : 'Limpar quarto', descricao: 'Dar a geral no quarto', responsavel: 'Jefferson',
-    //prioridade: 'Alta', data: '08/05/2022' }
-  ];
+  // podemos criar a tarefa assim, ou no construtor abaixo   // Tarefa[] = [];
+  tarefas: Observable<Tarefa[]>;
+    //[ {_id: 1, titulo : 'Limpar quarto', descricao: 'Dar a geral no quarto', responsavel: 'Jefferson',
+    //prioridade: 'Alta', data: '08/05/2022' }  ];
+
   displayedColumns = ['titulo', 'descricao', 'responsavel', 'prioridade', 'data'];
 
   //tarefasService: TarefasService;
 
+
   constructor(private tarefasService: TarefasService) {
     // this.tarefas = [];
-    //this.tarefasService = new TarefasService();
-
+    // this.tarefasService = new TarefasService();
+     this.tarefas = this.tarefasService.list();
   }
 
   ngOnInit(): void {
-    this.tarefas = this.tarefasService.list();
+    // this.tarefas = this.tarefasService.list();
   }
 
 }
