60243c1823fc8219693b0915555ba73a78eb5d1b
JeffersonEstevo <jefferson.estevo@gmail.com>
2022-05-10 19:47:33 -0300

    adicionando valores no backend via form

--- src/app/tarefas/services/tarefas.service.ts
+++ src/app/tarefas/services/tarefas.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
-import { Tarefa } from '../model/tarefa';
 import { delay, first, tap } from 'rxjs/operators';
+
+import { Tarefa } from './../model/tarefa';
 
 @Injectable({
   providedIn: 'root'
@@ -22,12 +22,19 @@ export class TarefasService {
     return this.httpClient.get<Tarefa[]>(this.API)
     .pipe(
       first(), // para obter a primeira resposta do servidor vindo do tarefas.json
-      delay(3000),
+      delay(2000),
       tap(tarefas => console.log(tarefas))
     );
     /*return [
       { "_id": 1, "titulo": "Limpar quarto", "descricao": "Dar a geral no quarto", "responsavel": "Jefferson",
     "prioridade": "Alta", "data": "08/05/2022" }
     ];*/
+
   }
+
+  // enviando dados vai form para a  API
+  save(record: Tarefa){
+    return this.httpClient.post<Tarefa>(this.API, record);
+  }
+
 }
--- src/app/tarefas/tarefa-form/tarefa-form.component.ts
+++ src/app/tarefas/tarefa-form/tarefa-form.component.ts
@@ -1,5 +1,9 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { TarefasService } from './../services/tarefas.service';
 
 @Component({
   selector: 'app-tarefa-form',
@@ -10,7 +14,10 @@ export class TarefaFormComponent implements OnInit {
 
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder,
+    private service: TarefasService,
+    private snackBar: MatSnackBar,
+    private location: Location) {
     this.form = this.formBuilder.group({
       titulo: [null],
       descricao: [null],
@@ -24,11 +31,20 @@ export class TarefaFormComponent implements OnInit {
   }
 
   onSubmit(){
-
+    this.service.save(this.form.value).subscribe(result => this.onSuccess(), error => this.onError());
   }
 
   onCancel(){
+    this.location.back();
+  }
 
+  private onSuccess(){
+    this.snackBar.open('Tarefa salva com sucesso!', '', {duration: 3000} );
+    this.onCancel();
+  }
+
+  private onError(){
+    this.snackBar.open('Erro ao salvar tarefa.', '', {duration: 3000} );
   }
 
 }
